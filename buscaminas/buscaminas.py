296141c6acf5e538173f3b65165b4512f02ec3b8
Mariana Chavez <141682590+Mariana-Chavez@users.noreply.github.com>
2023-12-05 13:40:42 -0600

    Update buscaminas.py

--- buscaminas/buscaminas.py
+++ buscaminas/buscaminas.py
@@ -5,46 +5,50 @@ import random
 import time
 
 
-root=Tk()
-frame=Frame(root)
+root = Tk()
+frame = Frame(root)
 frame.pack()
 root.title("Buscaminas")
 root.iconbitmap("img/bomba.ico")
 root.resizable(False, False)
 frame.config(width=400, height=400)
 #--------------Variables--------------
-bombasCerca=0
-win=False
-listaBotones=[]
-reset=False
-inicio=False
-varSlotPulsado=-1
-banderasDisponibles=10
-tiempoFin=0
-tiempoActual=0
-tiempoInicio=time.time()
-bandera=False
-tiempoHabilitado=False
-tomarTiempoFin=0
-y=0
+bombasCerca = 0 
+win = False
+listaBotones = []
+reset = False
+inicio = False
+varSlotPulsado = -1
+banderasDisponibles = 10
+tiempoFin = 0
+tiempoActual = 0
+tiempoInicio = time.time()
+bandera = False
+tiempoHabilitado = False
+tomarTiempoFin = 0
+y = 0
 
-contadorTiempo=Label(frame)
-contadorTiempo.grid(column=1, row=0, columnspan=4)
+contadorTiempo = Label(frame)
+contadorTiempo.grid(column = 1, row = 0, columnspan = 4)
 
 #------------Contador del tiempo-------------------
 def tiempo(tiempo1=""):
-	if tiempo1!=tiempo2 and tiempoHabilitado==True:
-		tiempo1=tiempo2
-		tiempoActual=int(tiempo2-tiempoInicio)
-		contadorTiempo.config(text="Tiempo transcurrido: " + str(tiempoActual), font=("Arial 15"))
-	else:
-		tiempo1=tiempo2
-		y+=1
-		if y==1:
-			tomarTiempoFin=int(tiempo2-tiempoInicio)
-			print("termino en: ", tomarTiempoFin)
-		contadorTiempo.config(text="Tiempo transcurrido: " + str(tomarTiempoFin), font=("Arial 15"))
-	contadorTiempo.after(200, tiempo)
+    global tiempoInicio, tiempoActual, inicio, tomarTiempoFin, tiempoHabilitado, y, tiempo2
+    tiempo2 = time.time()
+    if tiempo1 != tiempo2 and tiempoHabilitado == True:
+        tiempo1 = tiempo2
+        tiempoActual = int(tiempo2 - tiempoInicio)
+        contadorTiempo.config(text="Tiempo transcurrido: " + str(tiempoActual), font=("Arial 15"))
+    else:
+        tiempo1 = tiempo2
+        y += 1
+        if y == 1:
+            tomarTiempoFin = int(tiempo2 - tiempoInicio)
+            print("Terminó en: ", tomarTiempoFin)
+        contadorTiempo.config(text="Tiempo transcurrido: " + str(tomarTiempoFin), font=("Arial 15"))
+    contadorTiempo.after(200, tiempo)
+
+tiempo()
   
 #-------Generacion del tablero-------
 
