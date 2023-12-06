af387fc88a4b378f61890f66432c00160c98c872
Alma Patiño <60189112+emafriki@users.noreply.github.com>
2023-12-06 15:57:06 -0600

    Update buscaminas.py

--- buscaminas/buscaminas.py
+++ buscaminas/buscaminas.py
@@ -33,27 +33,38 @@ contadorTiempo.grid(column = 1, row = 0, columnspan = 4)
 
 #------------Contador del tiempo-------------------
 def tiempo(tiempo1=""):
-    global tiempoInicio, tiempoActual, inicio, tomarTiempoFin, tiempoHabilitado, y, tiempo2
+	""" Funcion para el contador de tiempo """
+    global tiempoInicio, tiempoActual, inicio, tomarTiempoFin, tiempoHabilitado, y
     tiempo2 = time.time()
-    if tiempo1 != tiempo2 and tiempoHabilitado == True:
+    if tiempo1 != tiempo2 and tiempoHabilitado:
         tiempo1 = tiempo2
         tiempoActual = int(tiempo2 - tiempoInicio)
-        contadorTiempo.config(text="Tiempo transcurrido: " + str(tiempoActual), font=("Arial 15"))
     else:
         tiempo1 = tiempo2
         y += 1
         if y == 1:
             tomarTiempoFin = int(tiempo2 - tiempoInicio)
-            print("Terminó en: ", tomarTiempoFin)
-        contadorTiempo.config(text="Tiempo transcurrido: " + str(tomarTiempoFin), font=("Arial 15"))
+            print("termino en:", tomarTiempoFin)
+    contadorTiempo.config(text="Tiempo transcurrido: " + str(tiempoActual if tiempoHabilitado else tomarTiempoFin),
+                          font=("Arial 15"))
     contadorTiempo.after(200, tiempo)
-  
+
 #-------Generacion del tablero-------
 
 def generarBotones():
-            for c in range(81): 
-		    
-                 listaBotones.append(Button(frame, width=6, height=3, text=" ", font=("Arial 12 bold"), command=lambda c=c:slotPulsado(c), bg="grey"))
+	""" Funcion para el generar el tablero"""
+    global listaBotones
+    rows, columns = 9, 9
+    button_width, button_height = 6, 3 	
+    for c in range(rows * columns):
+        row_position = (c // columns) + 1
+        col_position = (c % columns) + 1
+        button = Button(frame, width=button_width, height=button_height, text=" ", font=("Arial 12 bold"),
+                        command=lambda c=c: slotPulsado(c), bg="grey")
+        button.grid(column=col_position, row=row_position)
+        listaBotones.append(button)
+
+generarBotones()
 #---------------Bombas---------------
 bombas = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
