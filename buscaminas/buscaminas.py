e3e1210e526cdcbfa85ca053e820c4ad5f76fe65
Mariana Chavez <141682590+Mariana-Chavez@users.noreply.github.com>
2023-12-07 11:50:20 -0600

    Update buscaminas.py

--- buscaminas/buscaminas.py
+++ buscaminas/buscaminas.py
@@ -88,7 +88,8 @@ def mostrarBombas():
             listaBotones[bomba].config(image=imagenBomba, width=64, height=65)
             	
 def slot_pulsado(slot):
-    global bombas, lista_botones, bombas_cercanas, numero_pulsaciones, imagen_bomba, win, reset, var_slot_pulsado, inicio, bandera, tiempo_habilitado, tomar_tiempo_fin, contador_tiempo, tiempo_fin, tiempo2, tiempo_inicio
+    global bombas, lista_botones, bombas_cercanas, numero_pulsaciones, imagen_bomba, win, reset, var_slot_pulsado, inicio, bandera, tiempo_habilitado, tomar_tiempo_fin, contador_tiempo, tiempo_fin, tiempo2, tiempo_inicio, banderas_disponibles, bandera_img_slot, bandera_img
+
     numero_pulsaciones += 1
     bombas_cercanas = 0
     var_slot_pulsado = slot
@@ -99,6 +100,94 @@ def slot_pulsado(slot):
     else:
         inicio = True
         tiempo()
+
+    if not win:
+        if slot in bombas:
+            if bandera:
+                poner_bandera()
+            else:
+                mostrar_bombas()
+                lista_botones[slot].config(image=imagen_bomba, width=64, height=65, bg="#f17070")
+                tiempo_habilitado = False
+                reset = messagebox.askyesno("Game Over", "¿Desea volver a jugar?")
+                game_reset()
+        else:
+            def check():
+                nonlocal bombas_cercanas
+                neighbors = [slot + 1, slot - 1, slot + 9, slot - 9, slot - 8, slot + 8, slot + 10, slot - 10]
+                bombas_cercanas += sum(1 for neighbor in neighbors if neighbor in bombas)
+                print("abajo" if slot + 1 in bombas else "",
+                      "arriba" if slot - 1 in bombas else "",
+                      "derecha" if slot + 9 in bombas else "",
+                      "izquierda" if slot - 9 in bombas else "",
+                      "abajo a la izquierda" if slot - 8 in bombas else "",
+                      "arriba a la derecha" if slot + 8 in bombas else "",
+                      "abajo a la derecha" if slot + 10 in bombas else "",
+                      "arriba a la izquierda" if slot - 10 in bombas else "")
+
+            check()
+
+            if not bandera:
+                lista_botones[slot].config(text=bombas_cercanas, fg="black", font=("Arial 12 bold"))
+                lista_botones[slot].config(bg="#aeb0b2", state="disabled")
+            else:
+                poner_bandera()
+
+            def check_win():
+                nonlocal win
+                if slot not in bombas and numero_pulsaciones == 71:
+                    win = True
+                    txt_win = Label(frame, width=25, height=2, text="¡ G A N A S T E !", font=("helvetica 27 bold"), bg="#fe4a4a")
+                    txt_win.grid(row=10, column=1, columnspan=9)
+                    frame.config(bg="#fe4a4a")
+                    contador_tiempo.config(bg="#fe4a4a")
+                mostrar_bombas()
+
+            check_win()
+def presionar_bandera():
+    global bandera
+    bandera = True
+
+def poner_bandera():
+    global var_slot_pulsado, bandera, banderas_disponibles, contador_banderas, lista_botones, bandera_img_slot
+    if bandera and banderas_disponibles > 0:
+        banderas_disponibles -= 1
+        contador_banderas.config(text="Banderas disponibles: " + str(banderas_disponibles))
+        lista_botones[var_slot_pulsado].config(image=bandera_img_slot, width=64, height=65)
+        print("Bandera puesta en: ", var_slot_pulsado)
+    bandera = False
+
+contador_banderas = Label(frame, text="Banderas disponibles: " + str(banderas_disponibles), font=("Arial 15"))
+contador_banderas.grid(column=6, row=0, columnspan=5)
+
+boton_bandera = Button(frame, text=" ", image=bandera_img, command=presionar_bandera)
+boton_bandera.grid(column=5, row=0)
+
+def game_reset():
+    global reset, bombas_cercanas, win, lista_botones, tiempo_actual, tiempo_inicio, tiempo1, tiempo2, tiempo_actual, banderas_disponibles, bandera, y, tomar_tiempo_fin, tiempo_habilitado, numero_pulsaciones
+    if reset:
+        comienzo = False
+        bombas_cercanas = 0
+        win = False
+        lista_botones = []
+        bombas = [random.randrange(81) for _ in range(10)]
+        print("Las ubicaciones de las bombas son:", bombas)
+        numero_pulsaciones = 0
+        bombas_cercanas = 0
+        generar_botones()
+        tiempo_inicio = time.time()
+        tiempo()
+        banderas_disponibles = 10
+        contador_banderas.config(text="Banderas disponibles: " + str(banderas_disponibles))
+        bandera = False
+        reset = False
+    else:
+        root.destroy()
+
+# loop
+root.mainloop()
+
+
 		
     
 
